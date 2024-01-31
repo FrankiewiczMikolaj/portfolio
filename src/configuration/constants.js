1436e87b2b74d8f5773d34d0b5230aea93fbbcdf
Mikolaj <frankiewiczmikolaj@gmail.com>
2024-01-31 18:59:15 +0100

    Modified dates in constants.js

--- src/configuration/constants.js
+++ src/configuration/constants.js
@@ -88,7 +88,7 @@ export const schools = [
         img: "https://firebasestorage.googleapis.com/v0/b/portfolio-580cb.appspot.com/o/wlodkowic.jpg?alt=media&token=62581886-2fb0-4609-8042-eda6e9c706b0",
         title: "Pawel Wlodkowic University College in Plock",
         subtitle: "Programming & Databases",
-        date: "May 2021 - Present",
+        date: "Oct 2020 - Present",
         desc: "Aliquam vehicula non metus vitae vestibulum. Vestibulum eget tortor vestibulum, accumsan nisl eget, blandit eros. Donec interdum mauris et tellus rutrum, at posuere urna pharetra. Morbi eu sollicitudin nisi. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. In magna dolor, dictum eu ipsum at, elementum dignissim sapien. Cras porttitor purus sit amet ipsum lacinia, sit amet gravida libero pharetra. Nulla tempor accumsan nunc. Maecenas in pretium arcu. Donec vestibulum quam sapien.",
     },
     {
@@ -96,7 +96,7 @@ export const schools = [
         img: "https://firebasestorage.googleapis.com/v0/b/portfolio-580cb.appspot.com/o/zsn2.jpeg?alt=media&token=f3bdf40d-e5d7-41ae-8275-f9b255152b8f",
         title: "Leon Rutkowski Complex of Schools No. 2 in Płońsk",
         subtitle: "IT technician",
-        date: "Nov 2018 - Nov 2018",
+        date: "Sep 2016 - Apr 2020",
         desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur commodo mi leo, scelerisque pretium odio ornare sed. Mauris vitae tristique odio, et lacinia urna. Donec iaculis sagittis sapien at fermentum. Nam viverra turpis nec massa gravida feugiat. Nulla dictum urna sed dignissim efficitur. Sed tristique nibh sem, vitae viverra dui bibendum ac. Morbi vitae mi ac urna eleifend venenatis id et libero. Suspendisse potenti. Phasellus iaculis semper enim, eu dictum quam aliquam id. Aliquam tincidunt nibh eget suscipit pulvinar. Pellentesque bibendum eleifend lacus, a ultrices nibh vehicula eget.",
     },
 ];
